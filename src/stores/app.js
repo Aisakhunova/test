04e287ff1cf9ca0809ad199bb592adf1e90c2935
Aisakhunova <kamilla_ise@mail.ru>
2025-03-21 23:17:08 +0500

    ability to like items, empty fav page

--- src/stores/app.js
+++ src/stores/app.js
@@ -3,6 +3,22 @@ import { defineStore } from 'pinia'
 
 export const useAppStore = defineStore('app', {
 	state: () => ({
-		//
+		favorites: ref([])
 	}),
+
+	getters: {
+		isFavorite: (state) => (productId) => state.favorites.includes(productId),
+	  },
+	
+	actions: {
+		addToFavorites(productId) {
+			if (!this.favorites.includes(productId)) {
+			this.favorites.push(productId)
+			}
+		},
+
+		removeFromFavorites(productId) {
+			this.favorites = this.favorites.filter(id => id !== productId)
+		}
+	}
 })
