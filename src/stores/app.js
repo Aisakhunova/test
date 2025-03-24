d261f54c513cff7b539ad5fd99eba0b48741edfb
Aisakhunova <kamilla_ise@mail.ru>
2025-03-24 18:58:12 +0500

    cart functionality added

--- src/stores/app.js
+++ src/stores/app.js
@@ -3,22 +3,49 @@ import { defineStore } from 'pinia'
 
 export const useAppStore = defineStore('app', {
 	state: () => ({
-		favorites: ref([])
+		favorites: ref([]),
+		cart: [],
 	}),
 
 	getters: {
 		isFavorite: (state) => (productId) => state.favorites.includes(productId),
+		cartTotal: (state) => state.cart.reduce((sum, item) => sum + item.quantity, 0),
+    	cartItems: (state) => state.cart
 	  },
 	
 	actions: {
 		addToFavorites(productId) {
 			if (!this.favorites.includes(productId)) {
-			this.favorites.push(productId)
+				this.favorites.push(productId)
 			}
 		},
 
 		removeFromFavorites(productId) {
 			this.favorites = this.favorites.filter(id => id !== productId)
+		},
+
+		
+		addToCart(productId) {
+			const item = this.cart.find(item => item.id === productId)
+			if (item) {
+			  item.quantity++
+			} else {
+			  this.cart.push({ id: productId, quantity: 1 })
+			}
+		  },
+	  
+		removeFromCart(productId) {
+			this.cart = this.cart.filter(item => item.id !== productId)
+		},
+	
+		updateCartItem(productId, quantity) {
+			const item = this.cart.find(item => item.id === productId)
+			if (item) {
+				item.quantity = quantity
+				if (item.quantity <= 0) {
+					this.removeFromCart(productId)
+				}
+			}
 		}
 	}
 })
