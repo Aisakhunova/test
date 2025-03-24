56d38eed99d574bb001f1d079722f8e83320eaa9
Aisakhunova <kamilla_ise@mail.ru>
2025-03-24 18:26:47 +0500

    list of liked products

--- src/pages/likes.vue
+++ src/pages/likes.vue
@@ -1,5 +1,51 @@
+<script setup>
+import { useAppStore } from '@/stores/app';
+import useProducts from '@/composables/useProducts';
+import ProductCard from '@/components/products/ProductCard.vue';
+import { computed, onMounted } from 'vue';
+
+const appStore = useAppStore();
+const { flags, products, getAllProducts } = useProducts();
+
+onMounted(async () => {
+  await getAllProducts()
+})
+
+const favoriteProducts = computed(() => 
+  products.value.filter(p => appStore.favorites.includes(p.id))
+);
+
+</script>
+
 <template>
-    <div>
-      
-    </div>
+  <div>
+    <h1>Избранные товары</h1>
+
+    <v-progress-circular 
+        v-if="flags.isLoading" 
+        indeterminate color="primary" 
+        size="50" 
+        class="w-100 my-4" 
+    />
+    
+    <v-container v-else>
+        
+      <v-row v-if="favoriteProducts.length">
+        <v-col v-for="product in favoriteProducts" :key="product.id" cols="12" sm="6" md="4">
+          <ProductCard :product="product" />
+        </v-col>
+      </v-row>
+
+      <p v-else>Вы ещё ничего не добавили в избранное.</p>
+    </v-container>
+
+    
+  </div>
 </template>
+
+<style scoped>
+h1 {
+  text-align: center;
+  margin-bottom: 20px;
+}
+</style>
